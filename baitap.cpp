593ed3591bffc44c0b31464926a064babb85e5ef
Jiamnguyen1304 <mk.minhkhoi1304@gmail.com>
2025-10-18 22:05:01 +0700

    Add files via upload

---
+++ baitap.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>//standard input output header file
+#include <math.h>
+#include <cctype>
+void main()
+{
+	/*printf_s("Hello World!!!\n");*/
+
+
+	/*printf_s("Hello World!!!\n");
+	printf_s("I am a student");*/
+
+
+	/*int a, b;
+	printf_s("Nhap a: ");
+	scanf_s("%d", &a);
+	printf_s("Nhap b: ");
+	scanf_s("%d", &b);
+	int kq = a + b;
+	printf_s("Ket qua la:%d", kq);*/
+
+
+	/*int a, b;
+	printf_s("Nhap a: ");
+	scanf_s("%d", &a);
+	printf_s("Nhap b: ");
+	scanf_s("%d", &b);
+	int kq = a + b;
+	printf_s("Ket qua la:%d", kq);*/
+
+
+	/*double a;
+	printf_s("Nhap a: ");
+	scanf_s("%lf", &a);
+	double kq = sin(a);
+	printf_s("Ket qua la:%lf", kq);*/
+
+
+//#define PI 3.14159
+//	float R = 1.25;
+//	float dientich;
+//	dientich = PI * R * R;
+//	printf_s("Hinh tron, ban kinh=%f\n", R);
+//	printf_s("Dien tich =%f", dientich);
+
+
+//File varSize.cpp
+	/*short delta = 9;
+	printf_s("Kich thuoc delta=%d\n", sizeof(delta));
+	printf_s("Kich thuoc kieu int=%d\n", sizeof(int));
+	printf_s("Kich thuoc kieu long=%d\n", sizeof(long));
+	printf_s("Kich thuoc kieu float=%d\n", sizeof(float));
+	printf_s("Kich thuoc kieu char=%d\n", sizeof(char));*/
+
+
+	/*bool bVal;
+	float x = 46.7F, y = 93.0F, z;
+	bVal = (x == y);
+	printf_s("%d\n", bVal);
+	bVal = (x < y);
+	printf_s("%d\n", bVal);
+	z = (x > y) * x + (x <= y) * y;
+	printf_s("%f\n", z);*/
+
+
+	/*char ch = 65;
+	printf_s("%c\n", ch);
+	ch = 'A';
+	printf_s("%c\n", ch);
+	printf_s("Nhap ch: ");
+	scanf_s("%c", &ch);
+	printf_s("ASCII code: %d\n", ch);
+	ch = ch - ('a' - 'A') * (ch >= 'a' && ch <= 'z');
+	printf_s("Uppercase: %c\n", ch);*/
+
+
+	/*int a;
+	printf_s("Moi ban nhap nam sinh: ");
+	scanf_s("%d", &a);
+	int tuoi = 2025 - a;
+	printf_s("So tuoi cua ban la: %d", tuoi);*/
+
+
+	/*char kytu;
+	printf_s("Moi ban nhap mot ky tu bat ky: ");
+	scanf_s("%c", &kytu);
+	char kytu_hoa = toupper(kytu);
+	printf_s("Ky tu in hoa cua ky tu %c la: %c", kytu, kytu_hoa);*/
+
+
+	/*long long n,a;
+	printf_s("Moi ban nhap so tien can quy doi (VND): ");
+	scanf_s("%lld", &n);
+	printf_s("Menh gia ban can uu tien quy doi la:\n");
+	scanf_s("%lld", &a);
+	int list[6] = { 500000,200000,100000,50000,20000,10000 };
+	int i = 0;
+	for (i = 0; i < 6; i++) {
+		long long menh_gia = list[i];
+		int so_to = n / menh_gia;
+		printf_s("%lld : %d\n", menh_gia, so_to);
+		n = n % menh_gia;
+	}*/
+
+}
