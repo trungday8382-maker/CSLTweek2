51e40dc7ad458d0d70164976b687826a4b2d9e06
MinhTrung <trungday8382@gmail.com>
2025-10-23 20:26:02 +0700

    add main.cpp cua Minh Trung

---
+++ main.cpp
@@ -0,0 +1,102 @@
+//File hello.c
+#include <stdio.h>
+#include <math.h>
+#include<iostream>
+#include <map>
+#include <unordered_map>
+using namespace std;
+
+void main()
+{
+	//printf_s("Hello World!!!");
+
+	//printf_s("Hello World!!!\n");
+	////printf_s("I am a student");
+
+	//int a = 2, b = 3;
+	//int kq = a + b;
+	//printf_s("Ket qua la: %d", kq);
+
+	//int a, b;
+	//printf_s("Nhap a: ");
+	//scanf_s("%d", &a);
+	//printf_s("Nhap b: ");
+	//scanf_s("%d", &b);
+	//int kq = a + b;
+	//printf_s("Ket qua la: %d", kq);
+
+	//double a;
+	//printf_s("Nhap a: ");
+	//scanf_s("%lf", &a);
+	//double kq = sin(a);
+	//printf("Ket qua la: %lf", kq);
+
+	//#define PI 3.14159
+//	float R = 1.25;
+//	float DienTich;
+//	DienTich = R * R * PI;
+//	printf_s("Hinh tron, ban kinh = %f \n", R);
+//	printf_s("Dien tich = %f", DienTich);
+
+//short Delta = 9;
+	//printf_s("Kich thuoc delta = %d \n", sizeof(Delta));
+	//printf_s("Kich thuoc kieu int = %d \n", sizeof(int));
+	//printf_s("Kich thuoc kieu long = %d \n", sizeof(long));
+	//printf_s("Kich thuoc kieu float = %d \n", sizeof(float));
+	//printf_s("Kich thuoc kieu char = %d \n", sizeof(char));
+
+	//bool bVal;
+	//float x = 46.7F, y = 93.0F , z;
+	//bVal = (x == y);
+	//printf("%d\n", bVal);
+	//bVal = (x < y);
+	//printf_s("%d\n", bVal);
+	//z = (x > y) * x + (x <= y) * y;
+	//printf_s("%f\n", z);
+
+	/*char ch = 65;
+	printf_s("%c\n", ch);
+	ch = 'A';
+	printf_s("%c\n", ch);
+	printf_s("Nhap ch: ");
+	scanf_s("%c", &ch);
+	printf_s("ASCII code: %d\n", ch);
+	ch = ch - ('a' - 'A') * (ch >= 'a' && ch <= 'z');
+	printf_s("Upper case: %c\n", ch);*/
+
+	//-------------EXCERCISE 1----------------------------//
+	//int n;
+	//cout << "Moi thay nhap nam sinh cua minh: ";
+	//cin >> n;
+	//int kq = 2025 - n;
+	//cout << "Vay hien tai thay " << kq << " tuoi" << endl;
+
+	//-------------EXCERCISE 2----------------------------//
+	//char c;
+	//cout << "Moi thay nhap chu cai viet thuong: ";
+	//cin >> c;
+	//c = c - ('a' - 'A');
+	//cout << "Chu cai in hoa ma thay da chon la: " << c << endl;
+
+
+	//-------------EXCERCISE 3----------------------------//
+	//long long money;
+	//cout << "Moi thay nhap so tien can chuyen doi: ";
+	//cin >> money;
+	//cout << "Moi thay nhap menh gia ma thay uu tien: ";
+	//long long top;
+	//cin >> top;
+	//map<long long, long long> menhgia;
+	//menhgia[top] += money / top;
+	//money %= top;
+	//long long a[] = { 500000,200000,100000,50000,20000,10000,5000,2000,1000 };
+	//long long i = 0;
+	//while (money) {
+	//	menhgia[a[i]] += money / a[i];
+	//	money %= a[i];
+	//	i++;
+	//}
+	//for (auto x : menhgia) {
+	//	cout << "So to tien menh gia " << x.first << " dong la: " << x.second << endl;
+	//}
+}
